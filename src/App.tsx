45625081495bc549c58a7fb864c476d8a5c11c60
BrackListed <cameta8900@gmail.com>
2026-05-21 17:52:07 +0800

    Added header

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from "./assets/Header";
+
+
+export default function App() {
+  return(
+    <div className="flex flex-col gap-5">
+      <Header/>
+    </div>
+  )
+}
---
+++ src/assets/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div className="w-full h-20 bg-zinc-900 border-b text-zinc-50 border-zinc-950 shadow-md flex items-center">
+        <img src = "/logo.png" alt = "Bracklisted Store Logo" className="h-40 auto object-contain"></img>
+    </div>
+  );
+}
