ee68d53d9dbd573d3fb205b622818ca069dd7797
BrackListed <cameta8900@gmail.com>
2026-05-22 16:59:33 +0800

    Fixed conflict between the add to cart button and the product popup

--- src/assets/Main.tsx
+++ src/assets/Main.tsx
@@ -36,19 +36,20 @@ export function ProductUI({isCartOpen, setisCartOpen, productClicked, setproduct
                 <img src = {selectedProduct?.img} className="w-full h-full object-contain p-4" />
             </div>
 
-            <div className="flex items-center justify-between mt-2 pt-2 border-t border-zinc-100">
+            <div className="flex items-center justify-evenly mt-2 pt-2 border-t border-zinc-100">
                 <span className="text-2xl font-bold text-zinc-900">₱{selectedProduct?.price}</span>
+                <button onClick = {() => {dispatch(addToCart(selectProduct))}}className="px-6 py-3 bg-zinc-900 hover:bg-zinc-800 text-white text-xs font-medium rounded duration-150 shadow-sm hover:cursor-pointer hover:outline-gray-400 hover:outline-2 hover:scale-105 transition-all">Add to Cart</button>
                 <button className="px-6 py-3 bg-emerald-600 hover:bg-emerald-500 text-white font-semibold rounded-lg shadow-md transition-all cursor-pointer">Buy Now</button>
             </div>
         </div>
         </div>} 
         <div className="flex gap-3">{productList.map((product: ProductType) => (
-            <div onClick = {() => {setproductClicked(true), dispatch(selectProduct(product.id))}}className="w-64 bg-white border border-zinc-200 rounded-lg p-4 mx-5 shadow-sm flex flex-col gap-3 hover:cursor-pointer">
-                <div className="w-full h-48 bg-zinc-100 rounded flex items-center justify-center overflow-hidden">
+            <div className="w-64 bg-white border border-zinc-200 rounded-lg p-4 mx-5 shadow-sm flex flex-col gap-3 hover:cursor-pointer">
+                <div onClick = {() => {setproductClicked(true), dispatch(selectProduct(product.id))}} className="w-full h-48 bg-zinc-100 rounded flex items-center justify-center overflow-hidden">
                     <img src = {product.img} className="w-full h-full object-cover" />
                 </div>
                 <div className="flex flex-col gap-1">
-                    <span className="text-zinc-900 font-medium text-lg truncate">{product.name}</span>
+                    <span onClick = {() => {setproductClicked(true), dispatch(selectProduct(product.id))}} className="text-zinc-900 font-medium text-lg truncate">{product.name}</span>
                     <div className="flex gap-2">
                         <span className="text-zinc-600 font-semibold text-base">₱{product.price}</span>
                         <button onClick = {() => {dispatch(addToCart(product))}}className="px-3 py-1.5 bg-zinc-900 hover:bg-zinc-800 text-white text-xs font-medium rounded duration-150 shadow-sm hover:cursor-pointer hover:outline-gray-400 hover:outline-2 hover:scale-105 transition-all">Add to Cart</button>
